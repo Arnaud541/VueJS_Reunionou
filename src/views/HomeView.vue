bcda70498e487b753035c746fd6ec90308176f6b
PaulGillon <pau.gillon0@gmail.com>
2023-03-29 18:01:05 +0200

    ok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <br/><br/>
       <h1 class="title">ReunioNou</h1>
       <br/><br/>
-      <router-link to="/createEvent" class="button">Créer un évènement</router-link><br><br/><br/>
+      <router-link to="/createEvent" class="button is-primary">Créer un évènement</router-link><br><br/><br/>
       Ajouter une description
     </div>
   </div>
