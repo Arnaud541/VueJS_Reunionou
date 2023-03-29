86aed371228867985f07254dab1557b6f358526b
PaulGillon <pau.gillon0@gmail.com>
2023-03-29 16:11:15 +0200

    InvitView ok

--- src/views/InvitView.vue
+++ src/views/InvitView.vue
@@ -1,5 +1,23 @@
+<script setup>
+import {useRouter} from 'vue-router';
+
+const router = useRouter();
+
+function validationFormulaire() {
+  console.log("Validation formulaire");
+  router.push('/');
+}
+</script>
+
 <template>
-    <div>
-        test
-    </div>
+    <form @submit.prevent="validationFormulaire">
+        <div>
+            Invitation<br/>
+            Inviter des gens inscrit sur la plateforme<br/>
+            <button>Ajouter un participant</button><br/><br/><br/>
+            Email du participant <input type="text"><br/><br/>
+            Inviter des gens hors de la plateforme<br/>
+            Url générée à partager : URL
+        </div>
+    </form>
 </template>
