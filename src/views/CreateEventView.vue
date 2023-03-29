5190630c8ff2dc60cf9a54d73a57730df16339c8
PaulGillon <pau.gillon0@gmail.com>
2023-03-29 17:20:41 +0200

    ok

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -10,17 +10,38 @@ function validationFormulaire() {
 </script>
 
 <template>
-    <div>
-        Créer un évènement
-    </div>
-    <form @submit.prevent="validationFormulaire">
-        <div>
-            Titre : <input type="text"><br/>
-            Description : <input type="text"><br/>
-            Adresse : <input type="text"><br/>
-            Ville : <input type="text"><br/>
-            Code Postal : <input type="text"><br/>
-            <button>Créer</button>
+    <div class="columns">
+        <div class="column is-4 is-offset-4">
+            <h1 class="title">Créer un évènement</h1>
+            <form class="box" @submit.prevent="validationFormulaire">
+                <div class="field">
+                    <label class="label">Titre :</label>
+                    <input class="input" type="text"><br/>
+                </div>
+
+                <div class="field">
+                    <label class="label">Description :</label>
+                    <input class="input" type="text"><br/>
+                </div>
+
+                <div class="field">
+                    <label class="label">Adresse :</label>
+                    <input class="input" type="text"><br/>
+                </div>
+                <div class="field">
+                    <label class="label">Ville :</label>
+                    <input class="input" type="text"><br/>
+                </div>
+                <div class="field">
+                    <label class="label">Code Postal :</label>
+                    <input class="input" type="text"><br/>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <button class="button is-primary">Créer</button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </template>
