9de70b4a9c160d8e8d4c11740dab312a7a162d64
PaulGillon <pau.gillon0@gmail.com>
2023-03-29 17:08:46 +0200

    ok

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <h1>ReunioNou</h1>
-    <router-link to="/createEvent" class="button">Créer un évènement</router-link><br>
-    Ajouter une description
+  <div class="columns">
+    <div class="column is-5 is-offset-5">
+      <br/><br/>
+      <h1 class="title">ReunioNou</h1>
+      <br/><br/>
+      <router-link to="/createEvent" class="button">Créer un évènement</router-link><br><br/><br/>
+      Ajouter une description
+    </div>
   </div>
 </template>
 
