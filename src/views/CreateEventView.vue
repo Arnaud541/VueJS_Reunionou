b08bd352f07999e9dd0c65569d56eea78d9032a9
PaulGillon <pau.gillon0@gmail.com>
2023-03-29 17:58:59 +0200

    ok

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -1,7 +1,16 @@
 <script setup>
+import {reactive} from 'vue'
 import {useRouter} from 'vue-router';
 
 const router = useRouter();
+
+let event = reactive({
+  titre: '',
+  desc: '',
+  adresse: '',
+  ville: '',
+  cp: ''
+})
 
 function validationFormulaire() {
   console.log("Validation formulaire");
@@ -16,25 +25,25 @@ function validationFormulaire() {
             <form class="box" @submit.prevent="validationFormulaire">
                 <div class="field">
                     <label class="label">Titre</label>
-                    <input class="input" type="text"><br/>
+                    <input class="input" type="text" v-model="event.titre"><br/>
                 </div>
 
                 <div class="field">
                     <label class="label">Description</label>
-                    <input class="input" type="text"><br/>
+                    <input class="input" type="text" v-model="event.desc"><br/>
                 </div>
 
                 <div class="field">
                     <label class="label">Adresse</label>
-                    <input class="input" type="text"><br/>
+                    <input class="input" type="text" v-model="event.adresse"><br/>
                 </div>
                 <div class="field">
                     <label class="label">Ville</label>
-                    <input class="input" type="text"><br/>
+                    <input class="input" type="text" v-model="event.ville"><br/>
                 </div>
                 <div class="field">
                     <label class="label">Code Postal</label>
-                    <input class="input" type="text"><br/>
+                    <input class="input" type="text" v-model="event.cp"><br/>
                 </div>
                 <div class="field">
                     <div class="control">
