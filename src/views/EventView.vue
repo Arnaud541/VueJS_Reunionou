d91b4c808e0b7eece88e939a7183420ab511ac42
PaulGillon <pau.gillon0@gmail.com>
2023-03-31 16:18:30 +0200

    ok

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,31 +1,40 @@
 <template>
-    <div class="box columns">
-        <div class="column is-half">
-            <div class="has-text-centered title">
-                Nom de l'évènement<br/>
-            </div>
-            Description de l'évènement<br/><br/> 
-            <div class="columns box">
-                <div class="column">
-                    Participants<br/>
-                    <div class="box">
-                        <ul>
-                            <li>Participant1 Refus</li>
-                            <li>Participant2 Participe</li>
-                            <li>Participant3 En attente</li>
-                        </ul>
-                    </div> 
-                </div>
-                <div class="column">
-                    <button class="button is-primary">Accepter</button><br/>
-                    <button class="button is-danger">Refuser</button>
-                </div>
-            </div>
+    <div class="box">
+        <div class="has-text-centered title">
+            Nom de l'évènement<br/>
         </div>
-        <div class="column">
-            <figure>
-                <img src="/public/assets/img/téléchargement.png">
-            </figure>
+        <div class="columns">
+            <div class="column is-6">
+                <div class="ml-3 mb-2 mr-6 has-text-justified">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus dui mi, vitae sagittis erat fringilla eu. Nullam tristique tellus et nisl pulvinar faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tincidunt sem quam, quis convallis justo vestibulum sit amet. Quisque blandit turpis.<br/><br/> 
+                </div>
+                <div class="columns">
+                    <div class="column box ml-5 mb-2 is-4">
+                        Participants:<br/><br/>
+                            <ul>
+                                <li>Participant1 Refus</li>
+                                <li>Participant2 Participe</li>
+                                <li>Participant3 En attente</li>
+                                <li>Participant4 Refus</li>
+                                <li>Participant5 En attente</li><br/>
+                            </ul> 
+                            <button class="button is-primary is-fullwidth">Accepter</button><br/>
+                            <button class="button is-danger is-fullwidth">Refuser</button>
+                    </div>
+                    <div class="column box ml-6 mb-2 is-6">
+                        Zone Commentaire<br/>
+                        Zone Commentaire<br/>
+                        Zone Commentaire<br/>
+                        Zone Commentaire<br/>
+                        Zone Commentaire<br/>
+                    </div>
+                </div>
+            </div>
+            <div class="column mt-5">
+                <figure>
+                    <img src="/public/assets/img/image.png">
+                </figure>
+            </div>
         </div>
     </div>
 </template>
