3713457247cb91befd6ab0a89490064e9834ef7e
PaulGillon <pau.gillon0@gmail.com>
2023-03-31 11:20:13 +0200

    ok

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,33 +1,31 @@
 <template>
-    <div class="column">
-        <div class="box">
-            <div class="column is-5">
-                <div class="has-text-centered title">
-                    Nom de l'évènement<br/>
+    <div class="box columns">
+        <div class="column is-half">
+            <div class="has-text-centered title">
+                Nom de l'évènement<br/>
+            </div>
+            Description de l'évènement<br/><br/> 
+            <div class="columns box">
+                <div class="column">
+                    Participants<br/>
+                    <div class="box">
+                        <ul>
+                            <li>Participant1 Refus</li>
+                            <li>Participant2 Participe</li>
+                            <li>Participant3 En attente</li>
+                        </ul>
+                    </div> 
                 </div>
-                Description de l'évènement<br/><br/>
-                <div class="columns box">
-                    <div class="column">
-                        Participants<br/>
-                        <div class="box">
-                            <ul>
-                                <li>Participant1 Refus</li>
-                                <li>Participant2 Participe</li>
-                                <li>Participant3 En attente</li>
-                            </ul>
-                        </div> 
-                    </div>
-                    <div class="column">
-                        <button class="button is-primary">Accepter</button><br/>
-                        <button class="button is-danger">Refuser</button>
-                    </div>
+                <div class="column">
+                    <button class="button is-primary">Accepter</button><br/>
+                    <button class="button is-danger">Refuser</button>
                 </div>
             </div>
-            <div class="column">
-                <figure class="">
-                    <img src="/public/assets/img/téléchargement.png">
-                </figure>
-            </div>
+        </div>
+        <div class="column">
+            <figure>
+                <img src="/public/assets/img/téléchargement.png">
+            </figure>
         </div>
     </div>
 </template>
