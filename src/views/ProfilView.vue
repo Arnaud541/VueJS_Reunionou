e5d706030134d38bfe3f88cc5d0a36114e237152
PaulGillon <pau.gillon0@gmail.com>
2023-03-31 10:47:51 +0200

    ok

--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -81,50 +81,6 @@ const router = useRouter();
             </div>
         </div>
 
-        <!-- Div pour les évènements que l'utilisateur a créé -->
-        <div class="box">
-            <h3 class="title is-4">Evènements que vous avez créé</h3>
-
-            <div class="is-flex is-flex-wrap-wrap">
-                <div class="card m-4">
-                    <div class="card-image">
-                        <figure class="image is-4by3">
-                            <img src="https://via.placeholder.com/800x600" alt="Image de l'évènement"/>
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">Titre de l'évènement</p>
-                            </div>
-                        </div>
-                        <div class="content">
-                            Description de l'évènement
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card m-4">
-                    <div class="card-image">
-                        <figure class="image is-4by3">
-                            <img src="https://via.placeholder.com/800x600" alt="Image de l'évènement"/>
-                        </figure>
-                    </div>
-                    <div class="card-content">
-                        <div class="media">
-                            <div class="media-content">
-                                <p class="title is-4">Titre de l'évènement</p>
-                            </div>
-                        </div>
-                        <div class="content">
-                            Description de l'évènement
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
         <!-- Div pour les évènements auxquels l'utilisateur a participé -->
         <div class="box">
             <h3 class="title is-4">Evènements auxquels vous avez participé</h3>
