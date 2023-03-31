88e50255307c837546af13353154e234e5daab90
StarCobra <damienpoirot@gmail.com>
2023-03-31 11:12:47 +0200

    Feature: update FooterBar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -5,60 +5,60 @@ import {reactive} from "vue";
 const router = useRouter();
 
 const state = reactive({
-  navbarOpen: false
+    navbarOpen: false
 });
 
 function logout() {
-  // Code de déconnexion
-  if (!confirm('Voulez-vous vraiment vous déconnecter ?')) return;
-  router.push('/connexion');
+    // Code de déconnexion
+    if (!confirm('Voulez-vous vraiment vous déconnecter ?')) return;
+    router.push('/connexion');
 }
 
 function isAuthenticated() {
-  // Code de connexion
-  router.push('/');
+    // Code de connexion
+    router.push('/');
 }
 </script>
 
 <template>
-  <nav class="navbar has-background-grey-lighter" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <router-link to="/" class="navbar-item">
-        <img src="/public/assets/img/map.png" alt="70">
-      </router-link>
-
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu"
-         @click="state.navbarOpen=!state.navbarOpen">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
-    <div class="navbar-menu " :class="{'is-active':state.navbarOpen}">
-      <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-
-            <router-link to="/profil" class="button is-primary">
-              <strong>Profil</strong>
-            </router-link>
-            <router-link to="/inscription" class="button is-primary">
-              <strong>Inscription</strong>
+    <nav class="navbar has-background-info-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <router-link to="/" class="navbar-item">
+                <img src="/public/assets/img/map.png" alt="70">
             </router-link>
 
-            <router-link to="/connexion" class="button is-light">
-              Connexion
-            </router-link>
-
-            <button class="button is-danger" v-if="isAuthenticated" @click="logout">
-              <strong>Se déconnecter</strong>
-            </button>
-
-          </div>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu"
+               @click="state.navbarOpen=!state.navbarOpen">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
         </div>
-      </div>
-    </div>
 
-  </nav>
+        <div class="navbar-menu " :class="{'is-active':state.navbarOpen}">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+
+                        <router-link to="/profil" class="button is-primary">
+                            <strong>Profil</strong>
+                        </router-link>
+                        <router-link to="/inscription" class="button is-primary">
+                            <strong>Inscription</strong>
+                        </router-link>
+
+                        <router-link to="/connexion" class="button is-light">
+                            Connexion
+                        </router-link>
+
+                        <button class="button is-danger" v-if="isAuthenticated" @click="logout">
+                            <strong>Se déconnecter</strong>
+                        </button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </nav>
 </template>
